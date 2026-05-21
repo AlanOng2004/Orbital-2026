29ca3edf4c360ea8a90439fabf2e3b992f87df21
AlanOng2004 <alancx73@gmail.com>
2026-05-21 15:16:30 +0800

    Added JWT

---
+++ backend/src/main/java/com/orbital/nusmaps/exception/AuthenticationException.java
@@ -0,0 +1,8 @@
+package com.orbital.nusmaps.exception;
+
+public class AuthenticationException extends RuntimeException {
+
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
